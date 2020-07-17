1973b16013726ade6d80eedec4440d5e7f843a8a
Shin Kobara <shin.kobara@gmail.com>
2020-07-17 12:16:05 -0500

    add 2019 and 2020 Jan-feb

--- pages/index.vue
+++ pages/index.vue
@@ -367,6 +367,48 @@
             </cms-plugin>
           </p>
         </b-col>
+
+<b-col sm="6" md="4" lg="3">
+          <video style="width:100%;" controls>
+            <source src="https://geo.gcoos.org/data/ssh/gom/ssh_gom_2019.mp4" type="video/mp4">
+            <source src="movie.ogg" type="video/ogg">
+          </video>
+          <p style="text-align: center;">2019</p>
+          <p style="text-align: center;">
+            <cms-plugin
+              alt="Link/Button - Download NetCDF File "
+              id="6300"
+              render-plugin="true"
+              title="Link/Button - Download NetCDF File"
+            >
+              <a
+                class="btn btn-success btn-sm"
+                href="https://geo.gcoos.org/data/ssh/gom/ssh_gom_2019.nc"
+                role="button"
+              >Download NetCDF File</a>
+            </cms-plugin>
+          </p>
+        </b-col>
+      </b-row>
+      <b-row>
+                <b-col sm="6" md="4" lg="3">
+                            <h2>2020</h2>
+          <p style="text-align: center;">Jan-Feb</p>
+          <p style="text-align: center;">
+            <cms-plugin
+              alt="Link/Button - Download NetCDF File "
+              id="6300"
+              render-plugin="true"
+              title="Link/Button - Download NetCDF File"
+            >
+              <a
+                class="btn btn-success btn-sm"
+                href="https://geo.gcoos.org/data/ssh/gom/ssh_gom_2020_jan-feb.nc"
+                role="button"
+              >Download NetCDF File (Jan-Feb)</a>
+            </cms-plugin>
+          </p>
+        </b-col>
       </b-row>
     </b-container>
   </div>
